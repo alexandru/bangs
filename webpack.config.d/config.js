568f233eb0c064465d7e187829e36b3003c02ac3
Alexandru Nedelcu <noreply@alexn.org>
2025-03-24 09:30:07 +0200

    Fix

--- webpack.config.d/config.js
+++ webpack.config.d/config.js
@@ -1,14 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-config.output.filename = "bangs.[contenthash:7].js";
+module.exports = function(config) {
+    config.output = config.output || {};
+    config.output.filename = "bangs.[contenthash:7].js";
 
-config.plugins.push(
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "../../../../build/processedResources/js/main/index.html"),
-    }),
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "../../../../build/processedResources/js/main/search/index.html"),
-        filename: 'search/index.html'
-    })
-);
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../../../../build/processedResources/js/main/index.html"),
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../../../../build/processedResources/js/main/search/index.html"),
+            filename: 'search/index.html'
+        })
+    );
+
+    return config;
+};
